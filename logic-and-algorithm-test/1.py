41abac056c7ddaa934247192c12de29df4ef8c6f
fikrikhoironn <fikrikf470@gmail.com>
2023-10-17 19:12:39 +0700

    feat: change answer for question 1

--- logic-and-algorithm-test/1.py
+++ logic-and-algorithm-test/1.py
@@ -4,13 +4,17 @@
 # ○ Ubahlah format string tersebut menjadi format penulisan biasa dengan mengkonversi
 # spasi menjadi “-“ (strip)
 
+def is_alphanumeric_or_space(char):
+    ascii_val = ord(char)
+    return (65 <= ascii_val <= 90) or (97 <= ascii_val <= 122) or (48 <= ascii_val <= 57) or ascii_val == 32
+
 def title_format(input_string):
-    cleaned_string = ''.join(char for char in input_string if char.isalnum() or char.isspace())
+    cleaned_string = ''.join(char for char in input_string if is_alphanumeric_or_space(char))
     title_string = cleaned_string.title()
     return title_string
 
 def normal_format(input_string):
-    cleaned_string = ''.join(char for char in input_string if char.isalnum() or char.isspace())
+    cleaned_string = ''.join(char for char in input_string if is_alphanumeric_or_space(char))
     normal_string = cleaned_string.lower().replace(' ', '-')
     return normal_string
 
@@ -21,3 +25,4 @@ normal_string = normal_format(input_string)
 print("Original String: ", input_string)
 print("Title Format: ", title_string)
 print("Normal Format: ", normal_string)
+
