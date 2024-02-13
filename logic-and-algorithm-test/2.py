e1e07c67c5cc5d0df870ad351c0221b3e6391724
fikrikhoironn <fikrikf470@gmail.com>
2024-02-13 15:34:20 +0700

    refactor: change variable name

--- logic-and-algorithm-test/2.py
+++ logic-and-algorithm-test/2.py
@@ -1,18 +1,18 @@
 # Diberikan sebuah string acak, hitunglah berapa jumlah setiap karakter yang ada dalam string
 # tersebut
 def count_characters(input_string):
-    char_count = {}
+    character_count = {}
     for char in input_string:
-        if char in char_count:
-            char_count[char] += 1
+        if char in character_count:
+            character_count[char] += 1
         else:
-            char_count[char] = 1
-    return char_count
+            character_count[char] = 1
+    return character_count
 
 input_string = input("Enter a string: ")
-char_count = count_characters(input_string)
+character_count = count_characters(input_string)
 
 print("Input String: ", input_string)
 print("Character Counts: ")
-for char, count in char_count.items():
+for char, count in character_count.items():
     print(char, "=", count)
