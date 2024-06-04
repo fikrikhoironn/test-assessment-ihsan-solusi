e9f6eebb1dd0abd995086b3a7d03a02826f42790
fikrikhoironn <fikrikf470@gmail.com>
2024-06-04 19:50:32 +0700

    feat: make improvement

--- test-skripsi/test-improvement.tsx
+++ test-skripsi/test-improvement.tsx
@@ -10,7 +10,7 @@ class DataManager<T> {
 	}
 
 	removeData(item: T): boolean {
-		const index = this.data.indexOf(item);
+		const index = this.data.findIndex((i) => i === item);
 		if (index > -1) {
 			this.data.splice(index, 1);
 			return true;
@@ -23,11 +23,25 @@ class DataManager<T> {
 	}
 
 	getAllData(): Array<T> {
-		return this.data;
+		return [...this.data];
 	}
 
 	sortData(compareFunction: (a: T, b: T) => number): void {
-		this.data.sort(compareFunction);
+		if (this.data.length > 1000) {
+			this.data.sort(compareFunction);
+		} else {
+			this.insertionSort(compareFunction);
+		}
+	}
+
+	private insertionSort(compareFunction: (a: T, b: T) => number): void {
+		for (let i = 1; i < this.data.length; i++) {
+			let j = i;
+			while (j > 0 && compareFunction(this.data[j - 1], this.data[j]) > 0) {
+				[this.data[j - 1], this.data[j]] = [this.data[j], this.data[j - 1]];
+				j--;
+			}
+		}
 	}
 }
 
@@ -38,7 +52,7 @@ dataManager.addData(8);
 
 console.log(dataManager.getAllData());
 dataManager.sortData((a, b) => a - b);
-console.log(dataData.getAllData());
+console.log(dataManager.getAllData());
 
 dataManager.removeData(3);
-console.log(dataData.getAllData());
+console.log(dataManager.getAllData());
